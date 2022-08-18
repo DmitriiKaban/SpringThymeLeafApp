8365302b9e365d14bebb46b1184ca3e1f6d6f404
Dmitrii Cravcenco <dima16kravchenko@gmail.com>
2022-08-18 09:23:19 +0300

    Initial commit

---
+++ src/main/java/com/example/springthymeleafapp/SpringThymeLeafAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springthymeleafapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeLeafAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeLeafAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springthymeleafapp/SpringThymeLeafAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springthymeleafapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeLeafAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
